ec55118d51d302087dedf826ee7b6330f3d50e14
LYluyu <luyu@luyudemacbook-pro.local>
2016-09-23 22:27:23 +0800

    项目初始化

---
+++ Live/Live/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Live
+//
+//  Created by luyu on 16/9/23.
+//  Copyright © 2016年 luyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
